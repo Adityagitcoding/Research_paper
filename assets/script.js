678fd9f41c61909d159f2b1fcbf93385acca1a15
satyam pandey <satyam.sp.pandey@example.com>
2022-06-26 23:37:44 +0530

    last

---
+++ assets/script.js
@@ -0,0 +1,253 @@
+window.addEventListener('scroll' ,function(){
+    let mynav=document.querySelector('nav');
+    if(window.pageYOffset>0){
+      mynav.classList.add('sticky');
+    }
+    else{
+      mynav.classList.remove('sticky')
+    }
+     })
+   function themevis(){  
+     let theme=document.getElementById("theme")
+     if(theme.style.display==="none"){
+     theme.style.display="block";
+   }
+   else{
+   theme.style.display="none";
+   }
+}
+function first_changer(){
+  let nav=document.getElementById("navbar")
+  let navitem=document.getElementById("navtextbox")
+  let navitem1=document.getElementById("navtextbox1")
+  let navitem2=document.getElementById("navtextbox2")
+  let btn1=document.getElementById("btn1")
+  let btn2=document.getElementById("btn2")
+  let btn3=document.getElementById("btn3")
+  let btn4=document.getElementById("btn4")
+  let rgt=document.getElementById("rgt")
+  let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+  nav.style.background="transparent"
+  navitem.style.backgroundColor="#dfdfdf"
+  navitem1.style.backgroundColor="#dfdfdf"
+  navitem2.style.backgroundColor="#dfdfdf"
+  btn1.style.backgroundColor="#dfdfdf"
+  btn2.style.backgroundColor="#dfdfdf"
+  btn3.style.backgroundColor="#dfdfdf"
+  btn4.style.backgroundColor="#dfdfdf"
+  rgt.style.backgroundColor="#dfdfdf"
+  cnt.style.backgroundColor="#dfdfdf"
+  cnt1.style.backgroundColor="#dfdfdf"
+  cnt2.style.backgroundColor="#dfdfdf"
+  cnt3.style.backgroundColor="#dfdfdf"
+}
+function second_changer(){
+  let nav=document.getElementById("navbar")
+  let navitem=document.getElementById("navtextbox")
+  let navitem1=document.getElementById("navtextbox1")
+  let navitem2=document.getElementById("navtextbox2")
+  let btn1=document.getElementById("btn1")
+  let btn2=document.getElementById("btn2")
+  let btn3=document.getElementById("btn3")
+  let btn4=document.getElementById("btn4")
+  let rgt=document.getElementById("rgt")
+  let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="mediumseagreen"
+   navitem.style.backgroundColor="mediumseagreen"
+   navitem1.style.backgroundColor="mediumseagreen"
+   navitem2.style.backgroundColor="mediumseagreen"
+   btn1.style.backgroundColor="mediumseagreen"
+   btn2.style.backgroundColor="mediumseagreen"
+   btn3.style.backgroundColor="mediumseagreen"
+   btn4.style.backgroundColor="mediumseagreen"
+   rgt.style.backgroundColor="mediumseagreen"
+   cnt.style.backgroundColor="mediumseagreen"
+   cnt1.style.backgroundColor="mediumseagreen"
+   cnt2.style.backgroundColor="mediumseagreen"
+   cnt3.style.backgroundColor="mediumseagreen"
+ }
+ function third_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+   let btn2=document.getElementById("btn2")
+   let btn3=document.getElementById("btn3")
+   let btn4=document.getElementById("btn4")
+   let rgt=document.getElementById("rgt")
+   let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="violet"
+   navitem.style.backgroundColor="violet"
+   navitem1.style.backgroundColor="violet"
+   navitem2.style.backgroundColor="violet"
+   btn1.style.backgroundColor="violet"
+   btn2.style.backgroundColor="violet"
+   btn3.style.backgroundColor="violet"
+   btn4.style.backgroundColor="violet"
+   rgt.style.backgroundColor="violet"
+   cnt.style.backgroundColor="violet"
+   cnt1.style.backgroundColor="violet"
+   cnt2.style.backgroundColor="violet"
+   cnt3.style.backgroundColor="violet"
+ }
+ function fourth_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+   let btn2=document.getElementById("btn2")
+   let btn3=document.getElementById("btn3")
+   let btn4=document.getElementById("btn4")
+   let rgt=document.getElementById("rgt")
+   let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="#FA8072"
+   navitem.style.backgroundColor="#FA8072"
+   navitem1.style.backgroundColor="#FA8072"
+   navitem2.style.backgroundColor="#FA8072"
+   btn1.style.backgroundColor="#FA8072"
+   btn2.style.backgroundColor="#FA8072"
+   btn3.style.backgroundColor="#FA8072"
+   btn4.style.backgroundColor="#FA8072"
+   rgt.style.backgroundColor="#FA8072"
+   cnt.style.backgroundColor="#FA8072"
+   cnt1.style.backgroundColor="#FA8072"
+   cnt2.style.backgroundColor="#FA8072"
+   cnt3.style.backgroundColor="#FA8072"
+ }
+ function fivth_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+  let btn2=document.getElementById("btn2")
+  let btn3=document.getElementById("btn3")
+  let btn4=document.getElementById("btn4")
+  let rgt=document.getElementById("rgt")
+  let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="#BDB76B"
+   navitem.style.backgroundColor="#BDB76B"
+   navitem1.style.backgroundColor="#BDB76B"
+   navitem2.style.backgroundColor="#BDB76B"
+   btn1.style.backgroundColor="#BDB76B"
+   btn2.style.backgroundColor="#BDB76B"
+   btn3.style.backgroundColor="#BDB76B"
+   btn4.style.backgroundColor="#BDB76B"
+   rgt.style.backgroundColor="#BDB76B"
+   cnt.style.backgroundColor="#BDB76B"
+   cnt1.style.backgroundColor="#BDB76B"
+   cnt2.style.backgroundColor="#BDB76B"
+   cnt3.style.backgroundColor="#BDB76B"
+ }
+ function sixth_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+   let btn2=document.getElementById("btn2")
+   let btn3=document.getElementById("btn3")
+   let btn4=document.getElementById("btn4")
+   let rgt=document.getElementById("rgt")
+   let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="#008080"
+   navitem.style.backgroundColor="#008080"
+   navitem1.style.backgroundColor="#008080"
+   navitem2.style.backgroundColor="#008080"
+   btn1.style.backgroundColor="#008080"
+   btn2.style.backgroundColor="#008080"
+   btn3.style.backgroundColor="#008080"
+   btn4.style.backgroundColor="#008080"
+   rgt.style.backgroundColor="#008080"
+   cnt.style.backgroundColor="#008080"
+   cnt1.style.backgroundColor="#008080"
+   cnt2.style.backgroundColor="#008080"
+   cnt3.style.backgroundColor="#008080"
+ }
+ function seventh_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+  let btn2=document.getElementById("btn2")
+  let btn3=document.getElementById("btn3")
+  let btn4=document.getElementById("btn4")
+  let rgt=document.getElementById("rgt")
+  let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="#708090"
+   navitem.style.backgroundColor="#708090"
+   navitem1.style.backgroundColor="#708090"
+   navitem2.style.backgroundColor="#708090"
+   btn1.style.backgroundColor="#708090"
+   btn2.style.backgroundColor="#708090"
+   btn3.style.backgroundColor="#708090"
+   btn4.style.backgroundColor="#708090"
+   rgt.style.backgroundColor="#708090"
+   cnt.style.backgroundColor="#708090"
+   cnt1.style.backgroundColor="#708090"
+   cnt2.style.backgroundColor="#708090"
+   cnt3.style.backgroundColor="#708090"
+ }
+ function eighth_changer(){
+  let nav=document.getElementById("navbar")
+   let navitem=document.getElementById("navtextbox")
+   let navitem1=document.getElementById("navtextbox1")
+   let navitem2=document.getElementById("navtextbox2")
+   let btn1=document.getElementById("btn1")
+  let btn2=document.getElementById("btn2")
+  let btn3=document.getElementById("btn3")
+  let btn4=document.getElementById("btn4")
+  let rgt=document.getElementById("rgt")
+  let cnt=document.getElementById("cnt")
+  let cnt1=document.getElementById("cnt1")
+  let cnt2=document.getElementById("cnt2")
+  let cnt3=document.getElementById("cnt3")
+   nav.style.backgroundColor="#117864"
+   navitem.style.backgroundColor="#117864"
+   navitem1.style.backgroundColor="#117864"
+   navitem2.style.backgroundColor="#117864"
+   btn1.style.backgroundColor="#117864"
+   btn2.style.backgroundColor="#117864"
+   btn3.style.backgroundColor="#117864"
+   btn4.style.backgroundColor="#117864"
+   rgt.style.backgroundColor="#117864"
+   cnt.style.backgroundColor="#117864"
+   cnt1.style.backgroundColor="#117864"
+   cnt2.style.backgroundColor="#117864"
+   cnt3.style.backgroundColor="#117864"
+ }
+
+  //  function chng(){
+  //   let mynav=document.querySelector('nav')
+  //   if(window.pageYOffset>0){
+  //     mynav.classList.add('sticky');
+  //   }
+  //   else{
+  //     mynav.classList.remove('sticky')
+  //   }
+  //  }
+  
